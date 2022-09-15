3416dc5ad5b814bc944546895375deb64460f5f8
Masoud Sh <zzmasoud@gmail.com>
2022-09-15 11:41:31 +0200

    Initial Commit

---
+++ ZZSegmentor/ZZSegmentor.h
@@ -0,0 +1,18 @@
+//
+//  ZZSegmentor.h
+//  ZZSegmentor
+//
+//  Created by Masoud Sheikh Hosseini on 9/15/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZZSegmentor.
+FOUNDATION_EXPORT double ZZSegmentorVersionNumber;
+
+//! Project version string for ZZSegmentor.
+FOUNDATION_EXPORT const unsigned char ZZSegmentorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZZSegmentor/PublicHeader.h>
+
+
