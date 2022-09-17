3b3dfc3a8fed27d523e425b8fb499bbb0e2d06f8
Masoud Sh <zzmasoud@gmail.com>
2022-09-17 10:09:03 +0200

    Add a helper factory method to prepare test items and SUT object

--- ZZSegmentorTests/ZZSTimeframeTests.swift
+++ ZZSegmentorTests/ZZSTimeframeTests.swift
@@ -32,6 +32,21 @@ final class ZZSTimeframeTests: XCTestCase {
     
     // - MARK: Helpers
     
+    private func makeSUT(_ numberOfItems: Int, start: Date, end: Date) -> ZZSTimeframe {
+        var items: [DateItem] = []
+        var startDate = start.addingTimeInterval(-2.days)
+        for _ in 0..<numberOfItems {
+            let begin = startDate.addingTimeInterval(Int.random(in: 1...4).hours + Int.random(in: 0...55).minutes)
+            let finish = begin.addingTimeInterval(Int.random(in: 0...4).hours) + Int.random(in: 0...55).minutes
+            
+            guard let item = ZZSItem(start: begin, end: finish) else { fatalError() }
+            items.append(item)
+            
+            startDate = finish.addingTimeInterval(Int.random(in: 1...3).hours + Int.random(in: 30...55).minutes)
+        }
+        
+        return ZZSTimeframe(items: items, start: start, end: end)
+    }
 }
 
 private extension Int {
