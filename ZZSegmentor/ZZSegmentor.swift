634881acb91a1353c24f74b2ee82e1f2592ff81e
Masoud Sh <zzmasoud@gmail.com>
2022-10-09 10:57:26 +0200

    Add more convenience initializers

--- ZZSegmentor/ZZSegmentor.swift
+++ ZZSegmentor/ZZSegmentor.swift
@@ -9,8 +9,18 @@ import Foundation
 
 class ZZSegmentor {
     private var timeframe: Timeframe
+    private var items: [DateItem]
     
-    init(timeframe: Timeframe) {
-        self.timeframe = timeframe
+    public init(items: [DateItem], start: Date, end: Date) {
+        self.timeframe = ZZSTimeframe(items: items, start: start, end: end)
+        self.items = items
+    }
+    
+    public convenience init(timeframe: Timeframe) {
+        self.init(items: timeframe.items, start: timeframe.start, end: timeframe.end)
+    }
+    
+    public convenience init(start: Date, end: Date) {
+        self.init(items: [], start: start, end: end)
     }
 }
