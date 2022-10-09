ddc7f558ec6fa007c4cb21878da13568302d3add
Masoud Sh <zzmasoud@gmail.com>
2022-10-09 10:47:50 +0200

    Move `DateItem` codes to its own file

---
+++ ZZSegmentor/ZZSItem.swift
@@ -0,0 +1,38 @@
+//
+//  ZZSItem.swift
+//  ZZSegmentor
+//
+//  Created by Masoud Sheikh Hosseini on 10/9/22.
+//
+
+import Foundation
+
+public protocol DateItem {
+    var start: Date { get }
+    var end: Date { get }
+    var duration: TimeInterval { get }
+    init?(start: Date, end: Date)
+}
+
+
+final public class ZZSItem: ZZSegmentor.DateItem {
+    public private(set) var start: Date
+    
+    public private(set) var end: Date
+    
+    public var duration: TimeInterval {
+        return end.timeIntervalSince(start)
+    }
+    
+    required public init?(start: Date, end: Date) {
+        guard start < end else { return nil }
+        self.start = start
+        self.end = end
+    }
+}
+
+extension ZZSItem: CustomStringConvertible {
+    public var description: String {
+        return "\(start) .... \(end)"
+    }
+}
--- ZZSegmentor/ZZSegmentor.swift
+++ ZZSegmentor/ZZSegmentor.swift
@@ -6,33 +6,3 @@
 //
 
 import Foundation
-
-public protocol DateItem {
-    var start: Date { get }
-    var end: Date { get }
-    var duration: TimeInterval { get }
-    init?(start: Date, end: Date)
-}
-
-
-final public class ZZSItem: ZZSegmentor.DateItem {
-    public private(set) var start: Date
-    
-    public private(set) var end: Date
-    
-    public var duration: TimeInterval {
-        return end.timeIntervalSince(start)
-    }
-    
-    required public init?(start: Date, end: Date) {
-        guard start < end else { return nil }
-        self.start = start
-        self.end = end
-    }
-}
-
-extension ZZSItem: CustomStringConvertible {
-    public var description: String {
-        return "\(start) .... \(end)"
-    }
-}
