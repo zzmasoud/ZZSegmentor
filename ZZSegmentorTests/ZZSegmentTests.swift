234bf8e730a524e67a6b5434840ae82fec7c1011
Masoud Sh <zzmasoud@gmail.com>
2022-10-20 15:09:21 +0200

    Introduce new model called `ZZSDateUnitShare`

---
+++ ZZSegmentorTests/ZZSegmentTests.swift
@@ -0,0 +1,24 @@
+//
+//  ZZSegment.swift
+//  ZZSegmentorTests
+//
+//  Created by Masoud Sheikh Hosseini on 10/20/22.
+//
+
+import XCTest
+import ZZSegmentor
+
+struct ZZSDateUnitShare {
+    let date: Date
+    let unit: Int
+    let duration: TimeInterval
+    
+}
+
+class ZZSegment {
+    
+}
+
+final class ZZSegmentTests: XCTestCase {
+    
+}
